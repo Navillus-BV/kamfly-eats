07633dd57da545b4933320ea986f02524399fa88
Tony Sullivan <tony.f.sullivan@outlook.com>
2020-07-08 17:02:01 +0200

    adding the basic eleventy plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,74 @@
+const navigationPlugin = require('@11ty/eleventy-navigation')
+const imagePlugin = require('@navillus/eleventy-plugin-image')
+const manifestPlugin = require('@navillus/eleventy-plugin-manifest')
+const pwaPlugin = require('eleventy-plugin-pwa')
+const seoPlugin = require('eleventy-plugin-seo')
+const svgContentsPlugin = require('eleventy-plugin-svg-contents')
+const { DateTime } = require('luxon')
+
+const build = require('./site/_data/build')
+const site = require('./site/_data/site.json')
+
 module.exports = function (eleventyConfig) {
+  /**
+   * Opts in to a full deep merge when combining the Data Cascade.
+   *
+   * @link https://www.11ty.dev/docs/data-deep-merge/#data-deep-merge
+   */
+
+  /**
+   * Add custom watch targets
+   *
+   * @link https://www.11ty.dev/docs/config/#add-your-own-watch-targets
+   */
+
+  /**
+   * Passthrough file copy
+   *
+   * @link https://www.11ty.dev/docs/copy/
+   */
+
+  /**
+   * Add filters
+   *
+   * @link https://www.11ty.dev/docs/filters/
+   */
+  eleventyConfig.addFilter('dateDisplay', (dateObj, format = 'LLL d, y') => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat(format)
+  })
+
+  /**
+   * Add plugins
+   *
+   * @link https://www.11ty.devo/docs/plugins/
+   */
+  eleventyConfig.addPlugin(navigationPlugin)
+  eleventyConfig.addPlugin(pwaPlugin)
+  eleventyConfig.addPlugin(svgContentsPlugin)
+  eleventyConfig.addPlugin(imagePlugin, {
+    input: 'static',
+    output: '_output',
+    include: ['/uploads/blocks/*.+(jpg|jpeg|png)'],
+    sizes: [224, 448, 896]
+  })
+  eleventyConfig.addPlugin(seoPlugin, {
+    title: site.name,
+    description: site.description,
+    url: site.url,
+    author: site.author,
+    image: site.images.social,
+    ogtype: 'website',
+    options: { titleDivider: '|' }
+  })
+  eleventyConfig.addPlugin(manifestPlugin, {
+    output: '_output',
+    name: site.name,
+    short_name: site.name,
+    icon: site.images.favicon
+  })
+
   return {
     templateFormats: ['md', 'njk', 'html', '11ty.js'],
 
---
+++ _output/service-worker.js
@@ -0,0 +1,41 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+
+workbox.core.setCacheNameDetails({prefix: "eleventy-plugin-pwa"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "about/index.html",
+    "revision": "b8c00ca13d374d337eabda9142ad7cf7"
+  },
+  {
+    "url": "index.html",
+    "revision": "96cd538997199362287f8fcfe3b23e21"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^.*\.(html|jpg|png|gif|webp|ico|svg|woff2|woff|eot|ttf|otf|ttc|json)$/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
