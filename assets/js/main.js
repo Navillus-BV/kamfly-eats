a14c9fc52b7733f1d6cfe521503d0ca5ce3cc2f8
Tony Sullivan <tony.f.sullivan@outlook.com>
2020-09-21 20:04:09 +0200

    adding modal for the pricing comparison calculator

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,55 @@
       }
     })
   }
+
+  const modals = document.getElementsByClassName('modal')
+  const modalTriggers = document.getElementsByClassName('modal-trigger')
+
+  function openModal(el) {
+    if (el) {
+      document.body.classList.add('modal-is-active')
+      el.classList.add('is-active')
+    }
+  }
+
+  function closeModals() {
+    document.body.classList.remove('modal-is-active')
+    for (let i = 0; i < modals.length; i++) {
+      modals[i].classList.remove('is-active')
+    }
+  }
+
+  if (modals.length > 0 && modalTriggers.length > 0) {
+    for (let i = 0; i < modalTriggers.length; i++) {
+      let modalTrigger = modalTriggers[i]
+      let modal = document.getElementById(
+        modalTrigger.getAttribute('aria-controls')
+      )
+      if (modal) {
+        modalTrigger.addEventListener('click', function (e) {
+          e.preventDefault()
+          if (modalTrigger.hasAttribute('aria-controls')) {
+            openModal(modal)
+          }
+        })
+      }
+    }
+  }
+
+  document.addEventListener('click', function (e) {
+    if (
+      e.target.classList.contains('modal') ||
+      e.target.classList.contains('modal-close-trigger')
+    ) {
+      e.preventDefault()
+      closeModals()
+    }
+  })
+
+  document.addEventListener('keydown', function (event) {
+    var e = event || window.event
+    if (e.keyCode === 27) {
+      closeModals()
+    }
+  })
 })()
