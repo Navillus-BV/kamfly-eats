c444341117466faee2730e2bcd7c127fe37f198d
Tony Sullivan <tony.f.sullivan@outlook.com>
2020-10-10 22:35:02 +0200

    Getting rid of Netlify's form landing page all together

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,10 +1,21 @@
 ;(function () {
-  const urlParams = new URLSearchParams(window.location.search)
+  const form = document.querySelector('form[name="contact"]')
+  const modal = document.getElementById('contact-modal')
 
-  if (urlParams.has('success')) {
-    const modal = document.querySelector('.modal')
-    if (modal) {
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    const formData = new FormData(talkForm)
+    fetch(talkForm.getAttribute('action'), {
+      method: 'POST',
+      headers: {
+        Accept: 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+      body: new URLSearchParams(formData).toString(),
+    }).then((res) => {
       modal.open()
-    }
-  }
+      form.reset()
+    })
+  })
 })()
