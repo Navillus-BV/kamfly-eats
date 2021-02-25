1f8e08cd30e359be06440939ba40e51d06d75aa7
Tony Sullivan <tony@kamfly.io>
2021-02-25 14:51:58 +0100

    removing Acuity Scheduler

--- src/utils/htmlmin.js
+++ src/utils/htmlmin.js
@@ -1,11 +1,11 @@
 const htmlmin = require('html-minifier')
 
 module.exports = function (content, outputPath) {
-  return outputPath.endsWith('.html')
+  return outputPath && outputPath.endsWith('.html')
     ? htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-      })
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+    })
     : content
 }
