7826520bd69a6f6a668e6e7fdd15871ff476a7bb
Tony Sullivan <tony@navillus.nl>
2021-04-06 17:25:07 +0200

    Adding hero section

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import Button, { ButtonType } from '$lib/Button.svelte';
+
+	export let title: string;
+	export let subtitle: string;
+	export let cta: CallToAction;
+</script>
+
+<div class="hero">
+	<h1>{title}</h1>
+
+	{#if subtitle}
+		<p>{subtitle}</p>
+	{/if}
+
+	{#if cta}
+		<a class="btn btn--primary" href={cta.href}>
+			<Button type={ButtonType.Primary}>
+				{cta.title}
+			</Button>
+		</a>
+	{/if}
+</div>
+
+<style>
+	.hero {
+		max-width: 55rem;
+		margin-left: auto;
+		margin-right: auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 0 var(--spacing-4);
+		text-align: center;
+	}
+
+	.hero > *:not(:last-child) {
+		margin-bottom: var(--spacing-12);
+	}
+
+	p {
+		font-size: var(--text-lg);
+	}
+
+	@media (min-width: 480px) {
+		.hero {
+			padding: 0 var(--spacing-8);
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,9 @@
-<h1>Home</h1>
+<script lang="ts">
+	import Hero from '$lib/Hero.svelte';
+</script>
+
+<Hero
+	title="Your restaurant. Your future. Your call."
+	subtitle="The digital experience your customers want. The freedom you deserve."
+	cta={{ href: 'contact', title: 'Get Started' }}
+/>
