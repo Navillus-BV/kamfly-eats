7e0b318c695c7be7435bc658e7184c88437d703a
Tony Sullivan <tony@navillus.nl>
2021-04-06 16:22:49 +0200

    adding the global styles - src/main.css is temp to make sure I don't lose old styles

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,4 +2,26 @@
 	import '$assets/css/app.css';
 </script>
 
-<slot />
+<svelte:head>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		flex: 1 0 0%;
+		padding-top: var(--spacing-24);
+		display: flex;
+		flex-direction: column;
+	}
+
+	main > :global(:not:last-child) {
+		margin-bottom: var(--spacing-24);
+	}
+</style>
