ef335b098eeb550dab81d674cfeed14023d81561
Tony Sullivan <tony@navillus.nl>
2021-04-07 14:56:25 +0200

    adding privacy & terms routes, reading from markdown with vite-plugin-markdown

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -1,1 +1,90 @@
-<h1>Privacy Policy</h1>
+<script lang="ts">
+	import { attributes, html } from '$assets/pages/privacy.md';
+
+	const { title, last_updated } = attributes;
+
+	const lastUpdated = last_updated && new Date(last_updated).toDateString();
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<article>
+	{#if last_updated}
+		<div class="legal__last-updated">Last Updated: {lastUpdated}</div>
+	{/if}
+
+	{@html html}
+</article>
+
+<style>
+	article {
+		max-width: 72rem;
+		margin: 0 auto;
+		padding: 0 1rem 2rem;
+	}
+
+	article :global(h2) {
+		font-size: 1.875rem;
+		margin-top: 1.5rem;
+		margin-bottom: 2rem;
+	}
+
+	article :global(h3) {
+		font-size: 1.5rem;
+		margin-top: 1.5rem;
+		margin-bottom: 1rem;
+	}
+
+	article :global(li),
+	article :global(p) {
+		margin: 0.5rem 0;
+		line-height: 1.5;
+	}
+
+	.legal__last-updated {
+		font-size: 0.875rem;
+		margin-bottom: 1rem;
+	}
+
+	article :global(.table-of-contents) {
+		margin: 1.5rem 0;
+		line-height: 1.625;
+	}
+
+	article :global(.table-of-contents ul) {
+		margin-bottom: 0.5rem;
+		font-size: 1.125rem;
+		font-weight: 500;
+		margin-bottom: 0.75rem;
+	}
+
+	article :global(.table-of-contents ul ul) {
+		margin-left: 1rem;
+	}
+
+	@media (min-width: 640px) {
+		article {
+			padding: 0 1.5rem 2rem;
+		}
+	}
+
+	@media (min-width: 768px) {
+		article {
+			padding: 0 2rem 3rem;
+		}
+	}
+
+	@media (min-width: 1024px) {
+		article {
+			padding: 0 2rem 4rem;
+		}
+	}
+
+	@media (min-width: 1280px) {
+		article {
+			padding: 0 0 4rem;
+		}
+	}
+</style>
--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,8 +1,49 @@
 const sveltePreprocess = require('svelte-preprocess');
 const static = require('@sveltejs/adapter-static');
 const imagetools = require('vite-imagetools');
+const mdPlugin = require('vite-plugin-markdown');
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+const markdownItToc = require('markdown-it-table-of-contents')
+const slugify = require('slugify')
 const { resolve } = require('path');
 const pkg = require('./package.json');
+
+/**
+ * Override default markdown library
+ */
+function removeExtraText(s) {
+    let newStr = String(s).replace(/New\ in\ v\d+\.\d+\.\d+/, '')
+    newStr = newStr.replace(/⚠️/g, '')
+    newStr = newStr.replace(/[?!]/g, '')
+    newStr = newStr.replace(/<[^>]*>/g, '')
+    return newStr
+}
+
+function markdownItSlugify(s) {
+    return slugify(removeExtraText(s), { lower: true, remove: /[:’'`,]/g })
+}
+
+const md = markdownIt({ html: true, breaks: true, linkify: true })
+    .use(markdownItAnchor, {
+		permalink: true,
+		slugify: markdownItSlugify,
+		permalinkBefore: false,
+		permalinkClass: 'direct-link',
+		permalinkSymbol: '',
+		level: [1, 2, 3, 4],
+    })
+    .use(markdownItToc, {
+		includeLevel: [2, 3],
+		slugify: markdownItSlugify,
+		format: function (header) {
+			return removeExtraText(header)
+		},
+		transformLink: function (link) {
+			// remove backticks from markdown code
+			return link.replace(/\%60/g, '')
+		},
+    });
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
@@ -20,7 +61,11 @@ module.exports = {
 
 		vite: {
 			plugins: [
-				imagetools({ force: true })
+				imagetools({ force: true }),
+				mdPlugin.plugin({
+					mode: [mdPlugin.Mode.HTML],
+					markdownIt: md
+				})
 			],
 			resolve: {
 				alias: {
