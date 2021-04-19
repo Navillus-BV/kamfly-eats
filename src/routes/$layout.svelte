0dcadf32062d2d7609a2181a2c3e7ab318b445b9
Tony Sullivan <tony@navillus.nl>
2021-04-19 15:01:13 +0200

    removing Covid pricing discount

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
 	import { browser } from '$app/env';
-	import AlertBanner from '$lib/AlertBanner.svelte';
 	import AppFooter from '$lib/shell/AppFooter.svelte';
 	import AppHeader from '$lib/shell/AppHeader.svelte';
 	import SEO from '$lib/seo/SEO.svelte';
 	import '$assets/css/app.css';
-
-	const alert = {
-		title: 'COVID-19 Pricing:',
-		description: "We're waiving our monthly fee until things start picking back up.",
-		cta: {
-			href: '/pricing#plans',
-			title: 'See our plans'
-		}
-	};
 
 	let menuOpen: boolean;
 
@@ -24,10 +14,6 @@
 </script>
 
 <SEO />
-
-{#if !menuOpen}
-	<AlertBanner {...alert} />
-{/if}
 
 <AppHeader bind:menuOpen />
 
